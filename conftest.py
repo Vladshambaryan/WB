106706548946047aceb4ff1c402da36dc88a4ff7
Vladimir Shambaryan <132017930+Vladshambaryan@users.noreply.github.com>
2025-07-05 00:01:11 +0400

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,33 +1,28 @@
 import pytest
 from playwright.sync_api import sync_playwright
+from pathlib import Path
 
-
-"""Фикстура для браузера на уровне сессии"""
+# Фикстура для браузера
 @pytest.fixture(scope="session")
 def browser():
     with sync_playwright() as p:
         browser = p.chromium.launch(
             headless=True,
-            slow_mo=1000 # Можно настроить скорость
+            slow_mo=1000
         )
         yield browser
         browser.close()
 
-
-"""Фикстура для страницы на уровне теста"""
+# Фикстура для страницы
 @pytest.fixture
 def page(browser):
-    context = browser.new_context(storage_state="wb_auth_state.json")
+    storage_path = Path(__file__).parent / "wb_auth_state.json"
+    context = browser.new_context(storage_state=str(storage_path))
     page = context.new_page()
     yield page
     context.close()
 
-
-"""Базовый URL магазина"""
+# Базовый URL
 @pytest.fixture(scope="function")
 def shop_url():
     return "https://www.wildberries.ru/catalog/elektronika/noutbuki-periferiya"
-
-
-
-
