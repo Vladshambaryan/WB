834e9af30a07d2ecfa4b69e925a830a8628dc882
PC <skachat30913@gmail.com>
2025-07-05 00:20:22 +0400

    test

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import pytest
+from playwright.sync_api import sync_playwright
+
+@pytest.fixture(scope="session")
+def browser():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False, slow_mo=1000)
+        yield browser
+        browser.close()
+
+@pytest.fixture
+def page(browser):
+    storage_path = Path(__file__).parent / "wb_auth_state.json"
+    print(f"Загружается storage_state из: {storage_path.resolve()}")
+    context = browser.new_context(storage_state=str(storage_path))
+    page = context.new_page()
+    yield page
+    context.close()
+
+@pytest.fixture(scope="function")
+def shop_url():
+    return "https://www.wildberries.ru/catalog/elektronika/noutbuki-periferiya"
