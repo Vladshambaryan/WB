36a9daf0a393d9f0390f9f6af243513a7ffce290
Vladimir Shambaryan <132017930+Vladshambaryan@users.noreply.github.com>
2025-07-06 21:42:46 +0400

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ from playwright.sync_api import sync_playwright
 def browser():
     with sync_playwright() as p:
         browser = p.chromium.launch(
-            headless=False,
+            headless=True,
             args=[
                 "--window-size=1920,1080",
                 "--start-maximized",
